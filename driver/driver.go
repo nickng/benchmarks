99db4f0326469bea25acf89099d4e22f037f6a65
Nicholas Ng <nickng@projectfate.org>
2015-08-26 17:13:10 +0100

    Ignore missing pprof.StartTrace & pprof.StopTrace

--- driver/driver.go
+++ driver/driver.go
@@ -38,7 +38,7 @@ var (
 	affinity  = flag.Int("affinity", 0, "process affinity (passed to an OS-specific function like sched_setaffinity/SetProcessAffinityMask)")
 	tmpDir    = flag.String("tmpdir", os.TempDir(), "dir for temporary files")
 	genSvg    = flag.Bool("svg", false, "generate svg profiles")
-	trace     = flag.String("trace", "", "write an execution trace to the named file after execution")
+//	trace     = flag.String("trace", "", "write an execution trace to the named file after execution")
 
 	BenchNum  int
 	BenchMem  int
@@ -81,19 +81,19 @@ func Main() {
 		return
 	}
 
-	if *trace != "" {
-		f, err := os.Create(*trace)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
-		}
-		defer f.Close()
-		if err := pprof.StartTrace(f); err != nil {
-			fmt.Fprintf(os.Stderr, "can't start tracing: %s\n", err)
-			os.Exit(1)
-		}
-		defer pprof.StopTrace()
-	}
+//	if *trace != "" {
+//		f, err := os.Create(*trace)
+//		if err != nil {
+//			fmt.Fprintln(os.Stderr, err)
+//			os.Exit(1)
+//		}
+//		defer f.Close()
+//		if err := pprof.StartTrace(f); err != nil {
+//			fmt.Fprintf(os.Stderr, "can't start tracing: %s\n", err)
+//			os.Exit(1)
+//		}
+//		defer pprof.StopTrace()
+//	}
 
 	res := f()
 
